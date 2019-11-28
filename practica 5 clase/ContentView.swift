45989cb18a161315a6bbb1456c4a116d9f281d3e
Rodrigo <rodrigolp7@gmail.com>
2019-11-28 13:25:41 +0100

    Initial Commit

---
+++ practica 5 clase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  practica 5 clase
+//
+//  Created by Rodrigo Lopez on 28/11/2019.
+//  Copyright © 2019 UPM. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
